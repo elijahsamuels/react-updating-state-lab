0a1bb87aa89000ee1bcd4e20188448c964cefad3
Elijah Samuels <elijahsamuels@gmail.com>
2021-03-21 00:18:45 -0600

    commit tree 4813e4b4be6471ff200ebd472da788a22502df9f

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,32 @@
-// Code DigitalClicker Component Here
+
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+
+	constructor() {
+		super();
+	
+		// Initial state is defined
+		this.state = {
+			timesClicked: 0
+		};
+	  }
+
+	handleClick = () => {
+		this.setState(previousState => {
+			return {
+				timesClicked: previousState.timesClicked + 1,
+			}
+		})
+	}
+
+	render() {
+		// console.log('3 render: ', this)
+		return (
+			<div>
+				<button label={this.state.timesClicked} onClick={this.handleClick}>{this.state.timesClicked}</button>
+			</div>
+			)
+	}
+}
+
